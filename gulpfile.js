f2eefcf36e2265362c8205db4280ffb1f33bac65
kalmare <kalmare01@gmail.com>
2018-08-27 15:55:36 +0900

    Included autoreload

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const electron = require('electron-connect').server.create();
 
 gulp.task('compile', () => {
     gulp.src('view/index.html').pipe(gulp.dest('build/'));
 
-    return gulp.src('app/**/*.js').pipe(babel()).pipe(gulp.dest('build/'));
+    return gulp.src('view/*.js').pipe(babel()).pipe(gulp.dest('build/'));
+});
+
+gulp.task('start', ['compile'], () => {
+    electron.start();
+
+    gulp.watch(['app/main.js'], electron.restart);
+
+    gulp.watch(['view/*.{js,html}'], ['compile']);
+    gulp.watch(['build/*.{js,html}'], electron.reload);
 });
