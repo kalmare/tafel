c968e05eb09ac93b34f27bd4b962ee1b1e76275f
kalmare <kalmare01@gmail.com>
2018-08-27 16:04:21 +0900

    Add support for autoreloading css

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const babel = require('gulp-babel');
 const electron = require('electron-connect').server.create();
 
 gulp.task('compile', () => {
-    gulp.src('view/index.html').pipe(gulp.dest('build/'));
+    gulp.src('view/*.{html,css}').pipe(gulp.dest('build/'));
 
     return gulp.src('view/*.js').pipe(babel()).pipe(gulp.dest('build/'));
 });
@@ -13,6 +13,6 @@ gulp.task('start', ['compile'], () => {
 
     gulp.watch(['app/main.js'], electron.restart);
 
-    gulp.watch(['view/*.{js,html}'], ['compile']);
-    gulp.watch(['build/*.{js,html}'], electron.reload);
+    gulp.watch(['view/*.{js,css,html}'], ['compile']);
+    gulp.watch(['build/*.{js,css,html}'], electron.reload);
 });
